e0a83b11a167476259fa08a7815ad4f513301599
KristinaDavis <k_davis@physics.ucsb.edu>
2019-02-22 16:23:43 -0800

    worked on the examples in class

--- PlotExample_ggplot2.R
+++ PlotExample_ggplot2.R
@@ -9,4 +9,51 @@ library(ggplot2)
 # Read in Data
 gapminder = read.csv("data/gapminder_data.csv",header = TRUE)
 
+# data subsetting
+gapminder[1,1]
 
+# look at row 1
+gapminder[1,]
+
+# look at row 700
+gapminder[700,]
+
+# preview top few lines
+head(gapminder)
+
+#preview last few files
+tail(gapminder)
+
+# preview middle few lines
+gapminder[900:910,]
+
+# call ggplot to create new plot
+# GLOBAL option, appliews to ALL
+# aes = aesthetic properties
+#  x and y locations
+#   looks for header names, nice!
+# geom- this layer tells how we want to visually represent data
+#  geom_point = scatterplot
+cnt = factor(gapminder[continent])
+ggplot(data=gapminder, aes(x=gdpPercap, y=lifeExp, by=country, col=continent))+ geom_point()
+
+# add column of countries as characters, instead of factors
+countries_chars = as.character(gapminder$country)
+gap2 = cbind(gapminder,countries_chars)
+
+# Logical of which countries are high GDP
+whichHighGDP = gapminder$gdpPercap>30000
+high = gapminder$country[whichHighGDP==TRUE]
+str(high)
+ggplot(data = gapminder$country[Kuwait])
+
+ggplot(data=gapminder, aes(x=year, y=lifeExp, by=country, col=continent))+geom_line(color="black")+ geom_point()
+
+# Now Overplot Stats
+# use scale function
+ggplot(data=gapminder, aes(x=gdpPercap, y=lifeExp, color=continent)) +
+  geom_point(alpha=0.5, size=1.5, shape=8) + 
+  scale_x_log10() + 
+  geom_smooth(method="lm")
+
+       
--- my_script.R
+++ my_script.R
@@ -1,1 +1,66 @@
 # Learning About dplyr package
+gapminder = read.csv("data/gapminder_data.csv")
+
+# subset of gapminder dataset
+gapminder[gapminder$continent=="Africa",]
+
+# find highest GDP in Asia
+asia = gapminder[gapminder$continent=="Asia",]
+high = asia[asia$gdpPercap>300000]
+
+
+# subset in base R
+mean(gapminder[gapminder$continent=="Africa","gdpPerCap"])
+
+
+# load our package
+library(dplyr)
+
+yr_ctry_gdp_africa = gapminder %<%
+  filter(continent=="Africa") %>%
+  select(year,country,gdpPercap)
+
+gdp_by_country = gapminder %>% 
+  group_by(country) %>%
+  filter(gdpPercap > 300000)
+
+gapminder %>%
+  filter(continent=="Asia") %>%
+  group_by(country)
+
+gapminder %>%
+  filter(continent=="Asia") %>%
+  filter(gdpPercap > 100000)
+
+
+# Finding Highest and Lowest Mean Life Expectancy by Country
+lifeexp_by_country = gapminder %>%
+  group_by(country) %>%
+  summarize(mean_life = mean(lifeExp)) 
+lifeexp_by_country %>%
+  filter(mean_life == min(mean_life))
+lifeexp_by_country %>%
+  filter(mean_life == max(mean_life)
+         
+         
+
+# Perform more than one function inside a summarize
+gapminder %>%
+  group_by(continent) %>%
+  summarise(
+    mean_life = mean(lifeExp), 
+    se_life = sd(lifeExp)/sqrt(n())
+  )
+      
+# use count to find the number of rows
+gapminder %>%
+  filter(year == 2002) %>%
+  count(continent)
+
+# using mutate
+
+gdp_pop_summary = gapminder %>%
+  mutate(gdb_billions = gdpPercap*pop/10^9) %>%
+  group_by(continent,year) %>%
+  summarise(mean_gdp_bill = mean(gdp_billions))
+head(gdp_pop_summary)
